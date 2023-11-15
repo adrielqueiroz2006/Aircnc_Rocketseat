32a55c06cc197f74ebde6d6e7d8d1b8336e8ce59
Adriel <adrielqueiro@gmail.com>
2023-11-15 19:56:57 -0300

    add backend-adriel

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,14 @@
-const express = require ('express');
-const app = express();
+const express = require("express")
+const mongoose = require("mongoose")
+const routes = require("./routes")
 
-app.get('/', (req, res) => {
-    return res.json({ message: "Hello World"})
-});
+const app = express()
 
-app.listen(7777);
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0.t5byljq.mongodb.net/omnistack9?retryWrites=true&w=majority"
+)
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
