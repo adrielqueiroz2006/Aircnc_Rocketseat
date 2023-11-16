c5775e7be2ff7526b9330aee8a48d97128d111e6
naoeokami <caviniluiz@gmail.com>
2023-11-15 23:14:14 -0300

    add backend-cavini

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
-const express = require("express")
+const express = require("express");
+const multer = require('multer');
+const uploadConfig = require('./config/upload'); 
+const SessionController = require('./controllers/SessionController');
+const SpotController = require("./controllers/SpotController");
 
 const routes = express.Router()
+const upload = multer(uploadConfig);
 
-routes.post("/users", (req, res) => {
-  return res.json(req.body)
-})
+routes.post("/sessions", SessionController.store);
+routes.post("/spots", upload.single('thumbnail'),SpotController.store);
 
 module.exports = routes
